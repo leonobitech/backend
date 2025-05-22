5cfe08e5807e8c3c79a896befe68cf869497da87
leonobitech <leonobitech@gmail.com>
2025-05-22 01:54:03 -0300

    Debuging traefik

--- repositories/core/src/routes/security.routes.ts
+++ repositories/core/src/routes/security.routes.ts
@@ -16,18 +16,18 @@ securityRoutes.get(
   "/verify-admin",
   // 🐞 Debug básico
   (req, res, next) => {
-    console.log("=== DEBUG /security/verify-admin ===");
+    /* console.log("=== DEBUG /security/verify-admin ===");
     console.log("Headers:", {
       cookie: req.headers.cookie,
       host: req.headers.host,
       "user-agent": req.headers["user-agent"],
-    });
+    }); */
     next();
   },
   authenticate,
   authorize(UserRole.Admin),
   (req, res) => {
-    console.log("→ DEBUG: Usuario autenticado y autorizado como admin ✅");
+    //console.log("→ DEBUG: Usuario autenticado y autorizado como admin ✅");
     res.status(HTTP_CODE.OK).send("✅ OK");
   }
 );
