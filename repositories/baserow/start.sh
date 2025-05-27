d585cc938d18349e393185f1441a6aef51526ef3
leonobitech <leonobitech@gmail.com>
2025-05-27 16:40:35 -0300

    Fix buid image baserow

--- repositories/baserow/start.sh
+++ repositories/baserow/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#init command
 echo "🚀 Iniciando Baserow custom con bind 0.0.0.0"
 
 # Forzar gunicorn a escuchar en 0.0.0.0:8000
@@ -9,9 +8,9 @@ export GUNICORN_CMD_ARGS="--bind=0.0.0.0:8000"
 # Verificar si se pasa el comando `start` (flujo principal)
 if [[ "$1" == "start" ]]; then
   echo "🔑 Ejecutando Baserow con comando: start"
-  exec /baserow/docker-entrypoint.sh start
+  exec /entrypoint.sh start
 else
   # Si no es start, pasar cualquier otro comando a docker-entrypoint
   echo "🔧 Ejecutando Baserow con comando: $@"
-  exec /baserow/docker-entrypoint.sh "$@"
+  exec /entrypoint.sh "$@"
 fi
