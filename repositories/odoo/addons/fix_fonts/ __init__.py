0abedf9f7bd81dfd9b8ea1999884d9df02dd9c63
leonobitech <leonobitech@gmail.com>
2025-06-19 18:49:47 -0300

    Setup Odoo Fonts Part35

--- repositories/odoo/addons/fix_fonts/ __init__.py
+++ repositories/odoo/addons/fix_fonts/ __init__.py
@@ -1,12 +1,17 @@
-# __init__.py
 import sys
+from odoo import models
 
-# Asegúrate que el patch está accesible
+# Aseguramos acceso al patch
 PATCH_PATH = "/etc/odoo/patch_fonts.py"
-if PATCH_PATH not in sys.path:
+if "/etc/odoo" not in sys.path:
     sys.path.append("/etc/odoo")
 
 try:
     import patch_fonts
 except Exception as e:
     print(f"⚠️ Error al aplicar patch_fonts: {e}")
+
+# Esto hace que Odoo sí cargue el módulo
+class FixFonts(models.AbstractModel):
+    _name = 'fix_fonts.hook'
+    _description = 'Hook para forzar carga del patch_fonts'
